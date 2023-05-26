489cc6e0d18b23962428b0fb905f6fbf9f99fa57
MaksWenta <134703028+MaksWenta@users.noreply.github.com>
2023-05-26 22:14:57 +0200

    .

--- Hackathon/Assets/Scrpits/RandomStruct.cs
+++ Hackathon/Assets/Scrpits/RandomStruct.cs
@@ -4,15 +4,57 @@ using UnityEngine;
 
 public class RandomStruct : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Districts Dystrykt;
+    string dystrykt;
+    public GameObject prefab;
+    private void OnMouseDown()
     {
-        
-    }
+       dystrykt = Dystrykt.District;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if(dystrykt == "Aetheria")
+        {
+            Vector2 spawnPosition = new Vector2(Random.Range(-2f, 2f), Random.Range(-1f, 1f));
+            Instantiate(prefab, spawnPosition, Quaternion.identity);
+        }
+        else if (dystrykt == "Neoventus")
+        {
+            Vector2 spawnPosition = new Vector2(Random.Range(2f, 4f), Random.Range(-1f, 1f));
+            Instantiate(prefab, spawnPosition, Quaternion.identity);
+        }
+        else if (dystrykt == "Zenithia")
+        {
+            Vector2 spawnPosition = new Vector2(Random.Range(2f, 6f), Random.Range(1f, 3f));
+            Instantiate(prefab, spawnPosition, Quaternion.identity);
+        }
+        else if (dystrykt == "Astra Prime")
+        {
+            Vector2 spawnPosition = new Vector2(Random.Range(-2f, 2f), Random.Range(1f, 3f));
+            Instantiate(prefab, spawnPosition, Quaternion.identity);
+        }
+        else if (dystrykt == "Stellara")
+        {
+            Vector2 spawnPosition = new Vector2(Random.Range(-6f, -2f), Random.Range(-1f, 1f));
+            Instantiate(prefab, spawnPosition, Quaternion.identity);
+        }
+        else if (dystrykt == "Cybersphere")
+        {
+            Vector2 spawnPosition = new Vector2(Random.Range(-2f, 2f), Random.Range(-1f, -3f));
+            Instantiate(prefab, spawnPosition, Quaternion.identity);
+        }
+        else if (dystrykt == "Quantumville")
+        {
+            Vector2 spawnPosition = new Vector2(Random.Range(-6f, -2f), Random.Range(-1f, -3f));
+            Instantiate(prefab, spawnPosition, Quaternion.identity);
+        }
+        else if(dystrykt == "Oktavia")
+        {
+            Vector2 spawnPosition = new Vector2(Random.Range(-6f, -2f), Random.Range(1f, 3f));
+            Instantiate(prefab, spawnPosition, Quaternion.identity);
+        }
+        else if(dystrykt == "Lunaris")
+        {
+            Vector2 spawnPosition = new Vector2(Random.Range(2f, 6f), Random.Range(-1f, -3f));
+            Instantiate(prefab, spawnPosition, Quaternion.identity);
+        }
     }
 }
